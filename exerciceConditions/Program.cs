53e7bd1cc79e94068aa9e46f102d9e6caa1aea61
Christ ASSANA <christassana@gmail.com>
2024-06-06 09:54:23 +0200

    Résolution

--- exerciceConditions/Program.cs
+++ exerciceConditions/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Veuillez entrer un nombre qui sera le minimum : ");
+        string numberMinAsString = Console.ReadLine();
+        int numberMin = Convert.ToInt32(numberMinAsString);
+
+        Console.Write("Veuillez entrer un nombre qui sera le maximum : ");
+        string numberMaxAsString = Console.ReadLine();
+        int numberMax = Convert.ToInt32(numberMaxAsString);
+
+        Console.Write("Veuillez entrer un nombre entre ces deux valeurs : ");
+        string numberChooseAsString = Console.ReadLine();
+        int numberChoose = Convert.ToInt32(numberChooseAsString);
+
+        if (numberChoose > numberMax)
+        {
+            Console.WriteLine("Votre nombre est superieur au maximum !");
+        }
+        else if (numberChoose < numberMin)
+        {
+            Console.WriteLine("Votre nombre est inferieur au minimum !");
+        }
+        else
+        {
+            Console.WriteLine("Bravo !");
+        }
+    }
+}
